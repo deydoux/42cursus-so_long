2dd1960232c34efbd1fc9405a12c421871cf09f5
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-02-21 13:55:23 +0100

    Check args

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,16 +6,31 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:08:20 by deydoux           #+#    #+#             */
-/*   Updated: 2024/01/17 16:31:47 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/21 13:54:05 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+static bool	valid_extention(char *path)
+{
+	size_t	len;
+
+	len = ft_strlen(path);
+	if (len < 4)
+		return (false);
+	return (!ft_strncmp(path + len - 4, ".ber", 4));
+}
+
+int	main(int argc, char **argv)
 {
 	t_win	win;
 
+	if (argc != 2 || !valid_extention(argv[1]))
+	{
+		ft_putstr_fd("Usage: ./so_long map.ber\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
 	win.mlx = mlx_init();
 	win.ptr = mlx_new_window(win.mlx, WINDOW_WIDTH, WINDOW_HEIGH, "so_long");
 	mlx_hook(win.ptr, destroy_notify_event, structure_notify_mask, close_window,
