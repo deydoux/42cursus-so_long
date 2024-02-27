242a4b45afa068af8ce28a1b2da16599c0491f27
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-02-27 16:23:45 +0100

    Fix open_images

--- sources/open_images.c
+++ sources/open_images.c
@@ -6,11 +6,17 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 07:14:52 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/27 15:52:23 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/27 16:19:49 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static bool	open_image(void *mlx, char *file, int size, void **img_ptr)
+{
+	*img_ptr = mlx_xpm_file_to_image(mlx, file, &size, &size);
+	return (!*img_ptr);
+}
 
 static void	throw_error(void *mlx, char *map_str)
 {
@@ -26,9 +32,9 @@ void	open_images(void *mlx, char *map_str, t_img *img)
 		"assets/exit.xpm", "assets/player/back.xpm", "assets/player/face.xpm",
 		"assets/player/move.xpm", "assets/player/side.xpm", "assets/wall.xpm",
 		NULL};
-	const void	*img_ptr[] = {img->collectible, img->empty, img->exit,
-		img->player_back, img->player_face, img->player_move, img->player_side,
-		img->wall};
+	const void	*img_ptr[] = {&img->collectible, &img->empty, &img->exit,
+		&img->player_back, &img->player_face, &img->player_move,
+		&img->player_side, &img->wall};
 	int			size;
 	size_t		i;
 
@@ -36,11 +42,10 @@ void	open_images(void *mlx, char *map_str, t_img *img)
 	i = 0;
 	while (files[i])
 	{
-		img_ptr[i] = mlx_xpm_file_to_image(mlx, (char *)files[i], &size, &size);
-		if (!img_ptr[i])
+		if (open_image(mlx, (char *)files[i], size, (void **)img_ptr[i]))
 		{
 			while (i--)
-				mlx_destroy_image(mlx, (void *)img_ptr[i]);
+				mlx_destroy_image(mlx, *(void **)img_ptr[i]);
 			throw_error(mlx, map_str);
 		}
 		i++;
