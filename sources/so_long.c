40a5b0052ca62cfd44737e3bdf0071061a40fb0d
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-02-28 14:38:06 +0100

    Fix memory leak on new window error

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:08:20 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/28 13:56:56 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/28 14:37:14 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@ static bool	valid_extention(char *path)
 	return (!ft_strncmp(path + len - 4, ".ber", 4));
 }
 
-static void	new_window(void *mlx, void **win)
+static void	new_window(void *mlx, char *map_str, void **win)
 {
 	*win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGH, WINDOW_TITLE);
 	if (!*win)
 	{
 		ft_putstr_fd("Error\nFailed to create window\n", STDERR_FILENO);
 		free_mlx(mlx);
+		free(map_str);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -49,7 +50,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Error\nFailed to connect to X server\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
-	new_window(game.mlx, &game.win);
+	new_window(game.mlx, game.map.str, &game.win);
 	mlx_hook(game.win, destroy_notify_event, structure_notify_mask,
 		close_window, &game);
 	mlx_hook(game.win, key_release_event, key_release_mask, key_release, &game);
