9e366024848521ddb0d905c9b98c66067d5acc5a
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-12 14:14:20 +0100

    Update so_long_params.h

--- include/so_long_params.h
+++ include/so_long_params.h
@@ -6,18 +6,18 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 10:52:26 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/11 10:53:05 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/12 14:13:24 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_PARAMS_H
 # define SO_LONG_PARAMS_H
 # define IMAGE_SIZE 64
-# define WINDOW_HEIGH 720
-# define WINDOW_WIDTH 1280
-# define WINDOW_TITLE "so_long"
 # define KEY_UP 'w'
 # define KEY_LEFT 'a'
 # define KEY_DOWN 's'
 # define KEY_RIGHT 'd'
+# define WINDOW_HEIGH 720
+# define WINDOW_WIDTH 1280
+# define WINDOW_TITLE "so_long"
 #endif
