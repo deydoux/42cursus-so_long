1bf20f66c6eeb400a271ed768bf9cfdd7e73515e
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-12 13:16:41 +0100

    Optimize moves

--- sources/init_hooks/loop.c
+++ sources/init_hooks/loop.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:36:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/11 15:04:17 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/12 12:04:03 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,16 @@ int	loop(t_game *game)
 {
 	static int	x = 0;
 	static int	y = 0;
-	bool		update;
+	bool		x_update;
+	bool		y_update;
 
-	update = false;
-	if (game->keys.up ^ game->keys.down)
-	{
-		update = true;
-		if (game->keys.up)
-			y++;
-		else
-			y--;
-	}
-	if (game->keys.left ^ game->keys.right)
-	{
-		update = true;
-		if (game->keys.left)
-			x++;
-		else
-			x--;
-	}
-	if (update)
+	x_update = game->keys.left ^ game->keys.right;
+	y_update = game->keys.up ^ game->keys.down;
+	if (x_update)
+		x += (2 - y_update) * (game->keys.left + (game->keys.right * -1));
+	if (y_update)
+		y += (2 - x_update) * (game->keys.up + (game->keys.down * -1));
+	if (x_update || y_update)
 		mlx_put_image_to_window(game->mlx, game->win, game->map.img.ptr, x, y);
 	return (0);
 }
