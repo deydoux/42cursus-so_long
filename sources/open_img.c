4a6da5cfc32f311684ed774a53523902321b2970
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-13 14:58:26 +0100

    Fix open_img norm

--- sources/open_img.c
+++ sources/open_img.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:03:02 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/12 20:17:41 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/13 14:57:44 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_img	open_img(void *mlx, const char *filename, int heigh, int width)
 	img.heigh = heigh;
 	img.width = width;
 	img.ptr = mlx_xpm_file_to_image(mlx, (char *)filename, &img.width,
-		&img.heigh);
+			&img.heigh);
 	if (img.ptr)
 		img.buffer = mlx_get_data_addr(img.ptr, &img.pixel_bits, &img.line_size,
 				&img.endian);
