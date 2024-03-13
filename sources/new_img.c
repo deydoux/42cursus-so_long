010e5dbb5a81b84e91e12642e1c412a43a91beba
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-13 15:41:20 +0100

    Safe new_img

--- sources/new_img.c
+++ sources/new_img.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:17:56 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/29 13:21:52 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/13 15:37:36 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ t_img	new_img(void *mlx, int heigh, int width)
 
 	img.heigh = heigh;
 	img.width = width;
-	img.ptr = mlx_new_image(mlx, width, heigh);
-	if (img.ptr)
-		img.buffer = mlx_get_data_addr(img.ptr, &img.pixel_bits, &img.line_size,
-				&img.endian);
+	if (heigh > INT_MAX / 32 || width > INT_MAX / 32)
+		img.ptr = NULL;
+	else
+	{
+		img.ptr = mlx_new_image(mlx, width, heigh);
+		if (img.ptr)
+			img.buffer = mlx_get_data_addr(img.ptr,
+					&img.pixel_bits, &img.line_size, &img.endian);
+	}
 	return (img);
 }
