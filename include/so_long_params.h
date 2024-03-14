e565418e517261d20338ee7d5338ebc7fc1747ee
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-14 13:36:11 +0100

    Safe new_img

--- include/so_long_params.h
+++ include/so_long_params.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 10:52:26 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/12 14:13:24 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/14 13:32:16 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # define KEY_LEFT 'a'
 # define KEY_DOWN 's'
 # define KEY_RIGHT 'd'
+# define MAX_IMG_DIMENSION 32767
+# define MAX_IMG_PIXEL 536107716
 # define WINDOW_HEIGH 720
 # define WINDOW_WIDTH 1280
 # define WINDOW_TITLE "so_long"
--- sources/new_img.c
+++ sources/new_img.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:17:56 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/13 15:37:36 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/14 13:17:38 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@ t_img	new_img(void *mlx, int heigh, int width)
 
 	img.heigh = heigh;
 	img.width = width;
-	if (heigh > INT_MAX / 32 || width > INT_MAX / 32)
-		img.ptr = NULL;
-	else
+	if (heigh > MAX_IMG_DIMENSION || width > MAX_IMG_DIMENSION
+		|| heigh * width > MAX_IMG_PIXEL)
 	{
-		img.ptr = mlx_new_image(mlx, width, heigh);
-		if (img.ptr)
-			img.buffer = mlx_get_data_addr(img.ptr,
-					&img.pixel_bits, &img.line_size, &img.endian);
+		img.ptr = NULL;
+		return (img);
 	}
+	img.ptr = mlx_new_image(mlx, width, heigh);
+	if (img.ptr)
+		img.buffer = mlx_get_data_addr(img.ptr,
+				&img.pixel_bits, &img.line_size, &img.endian);
 	return (img);
 }
