fd24f766cb71960d5bae1b4750a33df6dc272b91
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-22 17:48:16 +0100

    Optimize copy_img

--- sources/copy_img.c
+++ sources/copy_img.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:23:03 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/22 12:41:51 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/22 17:47:42 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,23 @@ static void	copy_pixel(char *src, char *dst)
 
 void	copy_img(t_img src, t_img dst, int x, int y)
 {
+	int	null_src_x;
 	int	src_x;
 	int	src_y;
 
-	src_y = 0;
+	null_src_x = (x < 0) * ft_abs(x);
+	src_y = (y < 0) * ft_abs(y);
 	while (src_y < src.heigh)
 	{
-		src_x = 0;
+		src_x = null_src_x;
+		if (y + src_y > dst.heigh)
+			return ;
 		while (src_x < src.width)
 		{
-			if (0 <= x + src_x && x + src_x <= dst.width
-				&& 0 <= y + src_y && y + src_y <= dst.heigh)
-				copy_pixel(&src.buffer[src_x * 4 + src_y * src.line_size],
-					&dst.buffer[(x + src_x) * 4 + (y + src_y) * dst.line_size]);
+			if (x + src_x > dst.width)
+				break ;
+			copy_pixel(&src.buffer[src_x * 4 + src_y * src.line_size],
+				&dst.buffer[(x + src_x) * 4 + (y + src_y) * dst.line_size]);
 			src_x++;
 		}
 		src_y++;
