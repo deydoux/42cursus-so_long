1e78483ccd1a16a9c48f5f59b1aca129580f4312
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-03-24 15:06:52 +0100

    Fix render_frame

--- sources/loop/render_frame.c
+++ sources/loop/render_frame.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:12:31 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/24 01:23:19 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/24 15:05:23 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,15 @@ void	render_frame(t_game game)
 {
 	t_pos	map_pos;
 
-	if (game.pos.x < 0)
+	map_pos.x = game.pos.x * -1 + (game.win.frame.width - IMAGE_SIZE) / 2;
+	map_pos.y = game.pos.y * -1 + (game.win.frame.heigh - IMAGE_SIZE) / 2;
+	if (map_pos.x > 0)
 		map_pos.x = 0;
-	else if (game.map.img.width - game.pos.x < game.win.frame.width)
-		map_pos.x = (game.map.img.width - game.win.frame.width) * -1;
-	else
-		map_pos.x = game.pos.x * -1;
-	if (game.pos.y < 0)
+	else if (map_pos.x < game.win.frame.width - game.map.img.width)
+		map_pos.x = game.win.frame.width - game.map.img.width;
+	if (map_pos.y > 0)
 		map_pos.y = 0;
-	else if (game.map.img.heigh - game.pos.y < game.win.frame.heigh)
-		map_pos.y = (game.map.img.heigh - game.win.frame.heigh) * -1;
-	else
-		map_pos.y = game.pos.y * -1;
+	else if (map_pos.y < game.win.frame.heigh - game.map.img.heigh)
+		map_pos.y = game.win.frame.heigh - game.map.img.heigh;
 	copy_img(game.map.img, game.win.frame, map_pos.x, map_pos.y);
 }
