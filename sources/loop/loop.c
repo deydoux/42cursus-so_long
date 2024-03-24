d2b1180b57082d424d44557e66f4160c0493a2cd
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-03-24 15:29:13 +0100

    Update render frame

--- sources/loop/loop.c
+++ sources/loop/loop.c
@@ -6,57 +6,11 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:36:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/24 01:23:37 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/24 15:27:19 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "loop.h"
-
-static char	get_player_direction(t_key key)
-{
-	static char	direction = 'd';
-
-	if ((key.u ^ key.d) + (key.l ^ key.r) == 1)
-	{
-		if (key.u ^ key.d)
-		{
-			if (key.u)
-				direction = 'u';
-			else
-				direction = 'd';
-		}
-		else if (key.l ^ key.r)
-		{
-			if (key.l)
-				direction = 'l';
-			else
-				direction = 'r';
-		}
-	}
-	else if (direction == 'u' && key.d)
-		direction = 'd';
-	else if (key.u)
-		direction = 'u';
-	return (direction);
-}
-
-static void	put_player(t_game game)
-{
-	char	direction;
-	t_img	sprite;
-
-	direction = get_player_direction(game.key);
-	if (direction == 'u')
-		sprite = game.spr.player_u[ft_abs(game.pos.y / IMAGE_SIZE % 2)];
-	else if (direction == 'l')
-		sprite = game.spr.player_l[ft_abs(game.pos.x / IMAGE_SIZE % 2)];
-	else if (direction == 'r')
-		sprite = game.spr.player_r[ft_abs(game.pos.x / IMAGE_SIZE % 2)];
-	else
-		sprite = game.spr.player_d[ft_abs(game.pos.y / IMAGE_SIZE % 2)];
-	copy_img(sprite, game.win.frame, game.win.frame.width / 2 - IMAGE_SIZE / 2,
-		game.win.frame.heigh / 2 - IMAGE_SIZE / 2);
-}
 
 int	loop(t_game *game)
 {
@@ -80,8 +34,6 @@ int	loop(t_game *game)
 				ft_printf("\r%u moves", ++game->moves);
 		}
 		render_frame(*game);
-		if (false)
-			put_player(*game);
 		mlx_put_image_to_window(game->mlx, game->win.ptr, game->win.frame.ptr,
 			0, 0);
 	}
--- sources/loop/render_frame.c
+++ sources/loop/render_frame.c
@@ -6,15 +6,59 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:12:31 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/24 15:05:23 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/24 15:27:24 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "loop.h"
 
+static char	get_player_direction(t_key key)
+{
+	static char	direction = 'd';
+
+	if ((key.u ^ key.d) + (key.l ^ key.r) == 1)
+	{
+		if (key.u ^ key.d)
+		{
+			if (key.u)
+				direction = 'u';
+			else
+				direction = 'd';
+		}
+		else if (key.l ^ key.r)
+		{
+			if (key.l)
+				direction = 'l';
+			else
+				direction = 'r';
+		}
+	}
+	else if (direction == 'u' && key.d)
+		direction = 'd';
+	else if (key.u)
+		direction = 'u';
+	return (direction);
+}
+
+static t_img	get_player_spr(t_game game)
+{
+	char	direction;
+
+	direction = get_player_direction(game.key);
+	if (direction == 'u')
+		return (game.spr.player_u[game.pos.y / IMAGE_SIZE % 2]);
+	else if (direction == 'l')
+		return (game.spr.player_l[game.pos.x / IMAGE_SIZE % 2]);
+	else if (direction == 'r')
+		return (game.spr.player_r[game.pos.x / IMAGE_SIZE % 2]);
+	else
+		return (game.spr.player_d[game.pos.y / IMAGE_SIZE % 2]);
+}
+
 void	render_frame(t_game game)
 {
 	t_pos	map_pos;
+	t_pos	player_pos;
 
 	map_pos.x = game.pos.x * -1 + (game.win.frame.width - IMAGE_SIZE) / 2;
 	map_pos.y = game.pos.y * -1 + (game.win.frame.heigh - IMAGE_SIZE) / 2;
@@ -26,5 +70,8 @@ void	render_frame(t_game game)
 		map_pos.y = 0;
 	else if (map_pos.y < game.win.frame.heigh - game.map.img.heigh)
 		map_pos.y = game.win.frame.heigh - game.map.img.heigh;
+	player_pos.x = game.pos.x + map_pos.x;
+	player_pos.y = game.pos.y + map_pos.y;
 	copy_img(game.map.img, game.win.frame, map_pos.x, map_pos.y);
+	copy_img(get_player_spr(game), game.win.frame, player_pos.x, player_pos.y);
 }
