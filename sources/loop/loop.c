f4a93d0b333a507eb8bf0d0908675ce31117f3cf
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-03-24 15:47:49 +0100

    Add speed params

--- sources/loop/loop.c
+++ sources/loop/loop.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:36:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/24 15:27:19 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/24 15:46:31 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int	loop(t_game *game)
 
 	if (game->destroy)
 		close_win(game);
-	if (i++ % 64 == 0
+	if (i % 64 == 0
 		&& ((game->key.l ^ game->key.r) || (game->key.u ^ game->key.d)))
 	{
 		if (game->key.l ^ game->key.r)
 		{
-			game->pos.x += game->key.r + (game->key.l * -1);
+			game->pos.x += (game->key.r + (game->key.l * -1)) * SPEED;
 			if (game->pos.x % IMAGE_SIZE == 0)
 				ft_printf("\r%u moves", ++game->moves);
 		}
 		if (game->key.u ^ game->key.d)
 		{
-			game->pos.y += game->key.d + (game->key.u * -1);
+			game->pos.y += (game->key.d + (game->key.u * -1)) * SPEED;
 			if (game->pos.y % IMAGE_SIZE == 0)
 				ft_printf("\r%u moves", ++game->moves);
 		}
