06202792b5bfcaa7813141a5f284b60d0fd0b8a4
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-25 18:05:57 +0100

    Add check_collect

--- sources/loop/loop.c
+++ sources/loop/loop.c
@@ -6,15 +6,35 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:36:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/25 13:03:55 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/25 18:06:22 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "loop.h"
 
+static void	check_collect(t_game game)
+{
+	t_collect	*collect;
+
+	while (game.map.collects)
+	{
+		collect = game.map.collects->content;
+		if (collect->active && collect->pos.x - IMAGE_SIZE < game.pos.x
+			&& game.pos.x < collect->pos.x + IMAGE_SIZE
+			&& collect->pos.y - IMAGE_SIZE < game.pos.y
+			&& game.pos.y < collect->pos.y + IMAGE_SIZE)
+		{
+			collect->active = false;
+			return ;
+		}
+		game.map.collects = game.map.collects->next;
+	}
+}
+
 int	loop(t_game *game)
 {
 	check_move(game);
+	check_collect(*game);
 	render_frame(*game);
 	mlx_put_image_to_window(game->mlx, game->win.ptr, game->win.frame.ptr, 0,
 		0);
