5a82a6ae2a494596573840d0ff22b1764e085204
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-03-25 18:21:07 +0100

    Add check_exit

--- sources/loop/loop.c
+++ sources/loop/loop.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:36:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/03/25 18:06:22 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/03/25 18:20:06 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,33 @@ static void	check_collect(t_game game)
 	}
 }
 
+static void	check_exit(t_game game)
+{
+	int		i;
+	t_list	*collects;
+
+	i = game.pos.x / IMAGE_SIZE + game.pos.y / IMAGE_SIZE * game.map.width;
+	if (game.map.str[i] == 'E'
+		|| (game.pos.x % IMAGE_SIZE != 0 && game.map.str[i + 1] == 'E')
+		|| (game.pos.y % IMAGE_SIZE != 0
+			&& game.map.str[i + game.map.width] == 'E'))
+	{
+		collects = game.map.collects;
+		while (collects)
+		{
+			if (((t_collect *)collects->content)->active)
+				return ;
+			collects = collects->next;
+		}
+		close_win(&game);
+	}
+}
+
 int	loop(t_game *game)
 {
 	check_move(game);
 	check_collect(*game);
+	check_exit(*game);
 	render_frame(*game);
 	mlx_put_image_to_window(game->mlx, game->win.ptr, game->win.frame.ptr, 0,
 		0);
